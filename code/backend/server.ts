a4a1d2cf45f8239cdc6aa3613d56b8e6e54955e0
Daniel311099 <danielfisaha@gmail.com>
2024-04-16 17:28:11 +0100

    added cors middleware

--- code/backend/server.ts
+++ code/backend/server.ts
@@ -1,30 +1,29 @@
 import express, { Express, Request, Response } from "express";
+const cors = require("cors");
 import { closeDB, loadWord, openDB } from "./db";
 
 const db = openDB();
 
-const app = express()
-const port = 3000
+const app = express();
+app.use(cors());
+const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+    res.send("Express + TypeScript Server");
 });
 
 app.get("/word", (req: Request, res: Response) => {
     loadWord(db, (err: any, row: any) => {
         if (err) {
-            res
-                .status(500)
-                .send(err.message);
+            res.status(500).send(err.message);
         }
         console.log(row);
         res.send(row.word);
     });
 });
 
-
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 
 process.on("SIGINT", () => {
