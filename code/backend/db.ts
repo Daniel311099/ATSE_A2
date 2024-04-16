3fe5cfddbc3c096d283cdf0d4c701744cab1bedf
Daniel311099 <danielfisaha@gmail.com>
2024-04-16 17:07:40 +0100

    version 1 backend

--- code/backend/db.ts
+++ code/backend/db.ts
@@ -1,10 +1,10 @@
 import sqlite3 from 'sqlite3';
-export const db = new sqlite3.Database('./mydatabase.db', (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Connected to the SQlite database.');
-});
+// export const db = new sqlite3.Database('./mydatabase.db', (err) => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+//   console.log('Connected to the SQlite database.');
+// });
 
 const DB_NAME = 'mydatabase.db';
 
