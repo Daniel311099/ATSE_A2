1d6ce6ccf6bdd67aa5ebf31d5621737e3cd13ddc
Daniel311099 <danielfisaha@gmail.com>
2024-04-19 21:56:44 +0100

    added support for selecting dificulty

--- code/frontend/app/src/App.tsx
+++ code/frontend/app/src/App.tsx
@@ -5,55 +5,76 @@ import ScoreAndAttempt from "./components/ScoreAndAttempt";
 import HangMan from "./components/Hangman";
 import Result from "./components/Result";
 
-const URL = "http://localhost:3000/word";
+// const URL = "http://localhost:3000/word";
 
 function App() {
     const [word, setWord] = useState("");
     const [guesses, setGuesses] = useState<string[]>([]);
-    const [score,setScore]=useState<number>(0);
-    const [attempt,setAttempt]=useState<number>(0);
-    const [win,setWin]=useState(false)
+    const [score, setScore] = useState<number>(0);
+    const [attempt, setAttempt] = useState<number>(0);
+    const [win, setWin] = useState(false);
+    const [difficulty, setDifficulty] = useState("Medium");
 
     useEffect(() => {
         fetchWord();
-    }, []);
+    }, [difficulty]);
 
-    const fetchWord=()=>{
-        setGuesses('')
-        setScore(0)
-        setAttempt(0)
-        setWin(false)
+    const fetchWord = () => {
+        setGuesses("");
+        setScore(0);
+        setAttempt(0);
+        setWin(false);
+        const URL = `http://localhost:3000/word?difficulty=${difficulty}`;
         fetch(URL)
             .then((res) => res.text())
             .then((data) => setWord(data))
             .catch((err) => console.error(err));
-    }
+    };
 
     return (
         <>
-         <ScoreAndAttempt
-          score={score}
-          wrongAttempt={attempt}
-          />
-          <HangMan wrongAttempt={attempt}/>
+            <ScoreAndAttempt score={score} wrongAttempt={attempt} />
+            <HangMan wrongAttempt={attempt} />
 
-          { (attempt>5 || win)?
-          <Result word={word} isWin={win} />
-          :(
-            <div style={{display:'flex',justifyContent:'center',width:'100%',flexDirection:'column',alignItems:'center'}}>
-            <WordDisplay word={word} guesses={guesses} setWin={setWin} />
-            <GuessInput 
-            word={word}
-            setGuesses={setGuesses} 
-            attempt={attempt}
-            score={score}
-            setAttempt={setAttempt}
-            setScore={setScore}
-             />
-            </div>
-          )
-          }
-            <button style={{display:'block',margin:'10px auto'}} onClick={fetchWord}>RESET</button>
+            <select defaultValue={'Medium'} onChange={e => setDifficulty(e.target.value)}>
+                <option value="Easy">Easy</option>
+                <option value="Medium">Medium</option>
+                <option value="Hard">Hard</option>
+            </select>
+
+            {attempt > 5 || win ? (
+                <Result word={word} isWin={win} />
+            ) : (
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        width: "100%",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <WordDisplay
+                        word={word}
+                        guesses={guesses}
+                        setWin={setWin}
+                    />
+                    <GuessInput
+                        word={word}
+                        setGuesses={setGuesses}
+                        attempt={attempt}
+                        score={score}
+                        setAttempt={setAttempt}
+                        setScore={setScore}
+                    />
+                </div>
+            )}
+            <button
+                style={{ display: "block", margin: "10px auto" }}
+                onClick={fetchWord}
+            >
+                RESET
+            </button>
         </>
     );
 }
